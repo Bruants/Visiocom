f7899c951d8b662e64ca920716c853f92fa0ccca
Alexis <alexis.vivier@iut-rodez.fr>
2020-04-17 18:47:05 +0200

    Ajout de la barre menu latérale

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
+import { HideButtonComponent } from './navigation/sidebar/hide-button/hide-button.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SidebarComponent,
+    HideButtonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
