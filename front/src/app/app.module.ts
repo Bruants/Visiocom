e402b87e311f22705a46bd5ef0f6a51ac4eceb18
Alexis <alexis.vivier@iut-rodez.fr>
2020-04-17 18:50:08 +0200

    Revert "Ajout de la barre menu latérale"
    This reverts commit f7899c951d8b662e64ca920716c853f92fa0ccca.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { SidebarComponent } from './navigation/sidebar/sidebar.component';
-import { HideButtonComponent } from './navigation/sidebar/hide-button/hide-button.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    SidebarComponent,
-    HideButtonComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
