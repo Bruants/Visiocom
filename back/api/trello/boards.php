b8f8b933f9a964908ff086d4733928003312b764
Damien Avetta-Raymond <damien.avetta@gmail.com>
2020-06-08 20:54:11 +0200

    boards no request

--- back/api/trello/boards.php
+++ back/api/trello/boards.php
@@ -1,5 +1,5 @@
 <?php
 include_once '../requestAPI.php';
 $request = new requestAPI();
-echo $request.curl_put_request("https://api.trello.com/1/members/me/boards?key=".$GLOBALS['apiKeyTrello']."&token=".$token);
+#echo $request.curl_put_request("https://api.trello.com/1/members/me/boards?key=".$GLOBALS['apiKeyTrello']."&token=".$token);
 ?>
